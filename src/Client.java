67a08839a290d181a725dcb89bb7d81f61c77898
Burhozturk <31951806+Burhozturk@users.noreply.github.com>
2018-10-05 19:34:00 +0200

    Initial commit

---
+++ src/Client.java
@@ -0,0 +1,11 @@
+public class Client
+{
+    public static void main(String[] args)
+    {
+        MyInteger.isEven();
+        MyInteger.isOdd();
+        MyInteger.isPrime();
+        MyInteger.parseInt();
+
+    }
+}
---
+++ src/MyInteger.java
@@ -0,0 +1,127 @@
+public class MyInteger
+{
+    int value=0;
+    String methodsValue="";
+    public MyInteger(int value)
+    {
+        value=this.value;
+    }
+    public int getter()
+    {
+        return getter();
+    }
+
+    public isEven()
+    {
+        methodsValue="even";
+        if(MyInteger=methodsValue)
+        {
+            return true;
+        }
+    }
+
+    public isOdd()
+    {
+        methodsValue="odd";
+        if(MyInteger=methodsValue)
+        {
+            return true;
+        }
+    }
+
+    public isPrime()
+    {
+        methodsValue="Prime";
+        if(MyInteger=methodsValue)
+        {
+            return true;
+        }
+
+    }
+
+    public static int isEven()
+    {
+        String method="even";
+        if(MyInteger=Method)
+        {
+            return true;
+        }
+
+    }
+
+    public static int isOdd()
+    {
+        String method="odd";
+        if(MyInteger=method)
+        {
+            return true;
+        }
+    }
+
+    public static int isPrime()
+    {
+        String method="prime";
+        if(MyInteger=method)
+        {
+            return true;
+        }
+    }
+
+    public static MyInteger isEven()
+    {
+        String method="Prime";
+        if (MyInteger=method)
+        {
+            return true;
+        }
+    }
+
+    public static MyInteger isOdd()
+    {
+        String method="Odd";
+        if(MyInteger=method)
+        {
+            return true;
+        }
+    }
+
+    public static MyInteger isPrime()
+    {
+        String method="prime";
+        if(MyInteger=method)
+        {
+            return true;
+        }
+    }
+
+    public int equals()
+    {
+        if(MyInteger=value)
+        {
+            return true;
+        }
+
+    }
+
+    public MyInteger equals()
+    {
+        if(MyInteger=value)
+        {
+            return true;
+        }
+    }
+
+    public static char[] parseInt()
+    {
+
+
+    }
+
+    public static String parseInt()
+    {
+
+    }
+
+
+
+}
